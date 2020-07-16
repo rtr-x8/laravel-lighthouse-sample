7ca01ff91c13ae9375134c82cfb6203be3520554
Ryutaro Matsuoka <matsuoka@aivick.co.jp>
2020-07-16 18:45:56 +0900

    ごりっとvueを追加した

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,60 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3000/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+import VueApollo from 'vue-apollo'
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+const app = new Vue({
+    el: '#app',
+    apolloProvider,
+});
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <h1>Posts</h1>
+        <ul>
+            <li v-for="post in posts">
+                <b>{{ post.title }}</b> - <i>{{ post.author.name }}</i>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import gql from 'graphql-tag'
+
+    export default {
+        apollo: {
+            query: gql`{
+                posts {
+                    title
+                    author {
+                        name
+                        id
+                    }
+                }
+             }`,
+        }
+    }
+</script>
